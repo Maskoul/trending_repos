327e25c0288aebe5b4c5990a385f70a4d1b8a0a1
Moe <moha1058@gmail.com>
2020-05-31 15:51:46 +0100

    Add Detail Fragment

---
+++ app/src/main/java/com/example/trendingrepos/ui/detail/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.trendingrepos.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trendingrepos.databinding.FragmentDetailBinding
+
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+}
